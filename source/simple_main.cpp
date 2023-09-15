5d51c7036bd5c0e94a734c0d655ba312fdc945a2
Austin Ferguson <ferguson.austin.j@gmail.com>
2023-09-15 11:26:33 -0400

    Change Agent directionality for example

--- source/simple_main.cpp
+++ source/simple_main.cpp
@@ -12,7 +12,7 @@
 int main()
 {
   cse491::MazeWorld world;
-  world.AddAgent<cse491::PacingAgent>("Pacer 1");
+  world.AddAgent<cse491::PacingAgent>("Pacer 1").SetPosition(2,1);
   world.AddAgent<cse491::PacingAgent>("Pacer 2").SetPosition(4,1);
   world.AddAgent<cse491::TrashInterface>("Interface");
   world.Run();
