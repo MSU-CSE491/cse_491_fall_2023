ea5af24e1126165f72dcd635a0c03b6f1198010f
YonesMussa <mussayon@msu.edu>
2023-09-25 12:54:44 -0400

    fixing the error

--- source/DataCollection/DateReceiver.hpp
+++ source/DataCollection/DateReceiver.hpp
@@ -1,17 +1,18 @@
 #include <vector>
+#include <core/Entity.hpp>
 
 namespace GroupTwo{
+
+    class Entity;
 
     class DataReceiver {
 
     public:
     std::vector<cse491::GridPosition> storage;
-    cse491::GridPosition positions;
-
         
-        DataReceiver() {
-            storage.push_back(positions);     
-        }
+        DataReceiver(const cse491::Entity& entity) {
+      storage.push_back(entity.GetPosition());
+    }
 
         friend std::ostream& operator<<(std::ostream& os, const DataReceiver& obj) {
         os << "MyClass data: " << obj.storage[0].GetX();
