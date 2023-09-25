ea5cdfb82f89538ff1451f5ce0c6da1dc707c967
Yousif Murrani <murraniy@msu.edu>
2023-09-25 13:59:30 -0400

    fixed test namespace

--- source/Agents/AgentTests/FollowingAgentTest.hpp
+++ source/Agents/AgentTests/FollowingAgentTest.hpp
@@ -3,9 +3,11 @@
 #include "../FollowingAgent.hpp"
 #include "../../core/GridPosition.hpp"
 
+using namespace walle;
+
 TEST_CASE( "Following Agent", "[agent]" ) {
 
-    cse491::FollowingAgent agent1(1, "Following Agent 1");
+    FollowingAgent agent1(1, "Following Agent 1");
     agent1.SetPosition(1.0, 1.0);
 
     REQUIRE( agent1.GetPosition().GetX() == 1.0 );
