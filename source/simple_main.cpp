ab08078edbd6529dac654f277d179353797f0b57
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2023-09-26 19:03:17 -0400

    Update simple_main.cpp
    changed one the pacingAgent to a GP agent
    
    Co-Authored-By: Rajmeet Singh Chandok <rajmeetchandok@gmail.com>
    Co-Authored-By: ssitu <57548627+ssitu@users.noreply.github.com>
    Co-Authored-By: JasonMSeguin <143659605+JasonMSeguin@users.noreply.github.com>

--- source/simple_main.cpp
+++ source/simple_main.cpp
@@ -9,12 +9,18 @@
 #include "Interfaces/TrashInterface.hpp"
 #include "Worlds/MazeWorld.hpp"
 
-int main()
-{
-  cse491::MazeWorld world;
-  world.AddAgent<cse491::PacingAgent>("Pacer 1").SetPosition(3,1);
-  world.AddAgent<cse491::PacingAgent>("Pacer 2").SetPosition(6,1);
-  world.AddAgent<cse491::TrashInterface>("Interface").SetProperty("char", '@');
+#include "Group7_GP_Agent/agent.hpp"
 
-  world.Run();
+int main() {
+    cse491::MazeWorld world;
+    world.AddAgent<cse491::PacingAgent>("Pacer 1").SetPosition(3, 1);
+
+    //GP agent
+    world.AddAgent<cowboys::GPAgent>("GP 1").SetPosition(1, 0).SetProperty("char", 'G');
+
+    // Human agent
+    world.AddAgent<cse491::TrashInterface>("Interface").SetProperty("char", '@');
+
+
+    world.Run();
 }
