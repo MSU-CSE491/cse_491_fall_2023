037609ee394857107acdab19bec705f59c4878fe
Ethan Rylko <ethan.rylko@gmail.com>
2023-09-26 20:36:03 -0400

    Added test server

---
+++ source/Interfaces/NetWorth/testserver.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+//#include "networkingworld.hpp"
+#include <SFML/Network.hpp>
+
+int main() {
+    sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+
+    std::cout << "success " << ip << std::endl;
+
+    sf::UdpSocket socket;
+
+    // bind the socket to a port
+    if (socket.bind(55002) != sf::Socket::Done)
+    {
+        std::cout << "Failure to bind socket" << std::endl;
+        return 1;
+    }
+
+    char data[100];
+    std::size_t received;
+
+    // UDP socket:
+    sf::IpAddress sender;
+    unsigned short port;
+    if (socket.receive(data, 100, received, sender, port) != sf::Socket::Done)
+    {
+        std::cout << "Failure to receive" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Received " << received << " bytes from " << sender << " on port " << port << std::endl;
+    for (int i = 0; data[i] && i < 100; i++) {
+        printf("%c", data[i]);
+    }
+
+    // send back
+    std::string message = "ping!";
+    if (socket.send(message.c_str(), message.size(), sender, port) != sf::Socket::Done)
+    {
+        std::cout << "Failure to receive" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
+
+
