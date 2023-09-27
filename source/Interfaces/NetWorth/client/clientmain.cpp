62f3371bc2d45911a83ab8cf3d162b0b5c5f15b2
Ethan Rylko <ethan.rylko@gmail.com>
2023-09-27 00:25:53 -0400

    Basic client

--- source/Interfaces/NetWorth/client/clientmain.cpp
+++ source/Interfaces/NetWorth/client/clientmain.cpp
@@ -10,16 +10,35 @@
 #include "../../../Worlds/MazeWorld.hpp"
 #include "networkingagent.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
   /**Connect to server using arguments provided (argc/argv)
    * Once connection is established, the server will create an agent
    * for the client to control, returning it through the initial response
    * **/
-  cse491::MazeWorld world;
-  world.AddAgent<cse491::netWorth::NetworkingAgent>("Pacer 1").SetPosition(3,1);
-  world.AddAgent<cse491::netWorth::NetworkingAgent>("Pacer 2").SetPosition(6,1);
-  world.AddAgent<cse491::TrashInterface>("Interface").SetProperty("char", '@');
+  if (argc != 3) {
+    std::cout << "Must have an argument for server IP and port\nUsage: ./client [IP] [port]" << std::endl;
+    return 1;
+  }
 
-  world.Run();
+  std::string IPstring(argv[1]);
+  sf::IpAddress serverIP(IPstring);
+  sf::UdpSocket socket;
+  unsigned short port = stoi(std::string(argv[2]));
+
+  std::string message = "ping!";
+  if (socket.send(message.c_str(), message.size() + 1, serverIP, port) != Socket::Status::Done) {
+    std::cout << "Could not connect to " << IPstring << " at port " << port << std::endl;
+    return 1;
+  } 
+  
+  sf::IpAddress sender;
+  size_t received;
+  char data[100];
+  if (socket.receive(data, 100, received, serverIP, port) != sf::Socket::Done)
+  {
+      std::cout << "Failure to receive" << std::endl;
+      return 1;
+  }
+  std::cout << data << std::endl;
 }
