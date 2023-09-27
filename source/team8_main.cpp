16713eb6ad78e2b85dbacdd5e2ca26a3e2746468
Evan Orvis <orviseva@msu.edu>
2023-09-27 14:00:36 -0400

    Reorganized team8_main.cpp file Removed unnecessary MazeWorld include and refactored how strength property is added

--- source/team8_main.cpp
+++ source/team8_main.cpp
@@ -7,17 +7,16 @@
 // Include the modules that we will be using.
 #include "Agents/PacingAgent.hpp"
 #include "Interfaces/TrashInterface.hpp"
-#include "Worlds/MazeWorld.hpp"
 #include "Worlds/ManualWorld.hpp"
 
 int main()
 {
   cse491_team8::ManualWorld world;
-  world.AddAgent<cse491::TrashInterface>("Interface").SetProperty("char", '@').SetProperty("Strength", 7);;
-  world.AddAgent<cse491::PacingAgent>("Pacer 1").SetPosition(3,1).SetProperty("Strength", 2);
-  world.AddAgent<cse491::PacingAgent>("Pacer 2").SetPosition(6,1).SetProperty("Strength", 4);;
-  world.AddAgent<cse491::PacingAgent>("Pacer 3").SetPosition(18,3).SetProperty("Strength", 6);;
-  world.AddAgent<cse491::PacingAgent>("Pacer 4").SetPosition(21,3).SetProperty("Strength", 8);;
+  world.AddAgent<cse491::TrashInterface>("Interface", "Strength", 7).SetProperty("char", '@');
+  world.AddAgent<cse491::PacingAgent>("Pacer 1", "Strength", 2).SetPosition(3,1);
+  world.AddAgent<cse491::PacingAgent>("Pacer 2", "Strength", 4).SetPosition(6,1);
+  world.AddAgent<cse491::PacingAgent>("Pacer 3", "Strength", 6).SetPosition(18,3);
+  world.AddAgent<cse491::PacingAgent>("Pacer 4", "Strength", 8).SetPosition(21,3);
 
   world.Run();
 }
