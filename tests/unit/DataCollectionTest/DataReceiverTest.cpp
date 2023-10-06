ee38f1e6ab4967e7189b9521b99ec0548fb38847
YonesMussa <mussayon@msu.edu>
2023-10-05 21:33:18 -0400

    adding the tests

---
+++ tests/unit/DataCollectionTest/DataReceiverTest.cpp
@@ -0,0 +1,15 @@
+/**
+ * This file is part of the Fall 2023, CSE 491 course project.
+ * @brief Unit tests for Data.hpp in source/core
+ **/
+
+// Catch2 
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
+
+// Class project
+#include "DataCollection/DataReceiver.hpp"
+
+TEST_CASE(){
+
+}
