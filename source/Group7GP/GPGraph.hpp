a838cf62781f31dfd2568925ebbadc4b66dc4d66
ssit <ssit5568@gmail.com>
2023-10-06 09:58:26 -0400

    Added function pointers to graph nodes

--- source/Group7GP/GPGraph.hpp
+++ source/Group7GP/GPGraph.hpp
@@ -7,18 +7,69 @@
 #include <memory>
 #include <algorithm>
 #include <unordered_map>
+#include <numeric>
 
 #include "../core/AgentBase.hpp"
 
 namespace cowboys
 {
+    using NodeFunction = double (*)(const std::vector<double> &);
+
+    /// @brief Sum all inputs.
+    /// @param inputs Vector of doubles representing the inputs.
+    /// @return The sum of all inputs.
+    double Sum(const std::vector<double> &inputs) { return std::accumulate(inputs.begin(), inputs.end(), 0.0); }
+
+    /// @brief Check if all inputs are not equal to 1.
+    /// @param inputs Vector of doubles representing the inputs.
+    /// @return 1 if all inputs are not equal to 0, 0 otherwise.
+    double And(const std::vector<double> &inputs)
+    {
+        for (auto &input : inputs)
+        {
+            if (input == 0.)
+                return 0.;
+        }
+        return 1.;
+    }
+
+    /// @brief Check if any of the inputs besides the first are equal to the first input.
+    /// @param inputs Vector of doubles representing the inputs.
+    /// @return 1 if any of the inputs besides the first are equal to the first input, 0 otherwise.
+    double AnyEq(const std::vector<double> &inputs)
+    {
+        for (size_t i = 1; i < inputs.size(); ++i)
+        {
+            if (inputs.at(0) == inputs.at(i))
+                return 1.;
+        }
+        return 0.;
+    }
+
+    /// @brief Check if the first input is equal to 0.
+    /// @param inputs Vector of doubles representing the inputs.
+    /// @return 1 if the first input is equal to 0 or there are no inputs, 0 otherwise.
+    double Not(const std::vector<double> &inputs) { return (inputs.size() == 0) || (inputs.at(0) == 0.) ? 1. : 0.; }
+
+    /// @brief Returns the input with index 0 if the condition (input with index 1) is not 0.
+    /// @param inputs Vector of doubles representing the inputs.
+    /// @return The value of the input with index 0 if the condition (input with index 1) is not 0, 0 otherwise.
+    double Gate(const std::vector<double> &inputs)
+    {
+        return inputs.at(1) != 0. ? inputs.at(0) : 0.;
+    }
+
     class GraphNode
     {
     protected:
+        /// The input nodes to this node.
         std::vector<std::shared_ptr<GraphNode>> inputs;
         std::vector<size_t> inputs_indices;
 
-        /// The output of this node.
+        /// The function that operates on the outputs from a node's input nodes.
+        NodeFunction function_pointer = nullptr;
+
+        /// The default output of this node.
         double output = 0.;
 
     public:
@@ -35,11 +86,37 @@ namespace cowboys
         std::string name;
 
         /// TODO: Cache outputs
-        virtual double GetOutput() const { return output; }
+        virtual double GetOutput() const
+        {
+            // Invoke function pointer if it exists
+            if (function_pointer != nullptr)
+            {
+                std::vector<double> input_values;
+                for (auto &input : inputs)
+                {
+                    input_values.push_back(input->GetOutput());
+                }
+
+                try
+                {
+                    return function_pointer(input_values);
+                }
+                catch (const std::out_of_range &e)
+                {
+                    return 0.0;
+                }
+            }
+
+            // Default output
+            return output;
+        }
+
+        void SetFunctionPointer(NodeFunction function) { function_pointer = function; }
 
         std::shared_ptr<GraphNode> GetInput(size_t input_idx) const
         {
-            if (inputs.size() == 0) {
+            if (inputs.size() == 0)
+            {
                 // Throw error if no inputs
                 throw std::runtime_error("No inputs");
             }
@@ -170,7 +247,8 @@ namespace cowboys
             return count;
         }
 
-        size_t GetLayerCount() const {
+        size_t GetLayerCount() const
+        {
             return layers.size();
         }
 
--- tests/unit/gp_agents/GPGraph.cpp
+++ tests/unit/gp_agents/GPGraph.cpp
@@ -29,6 +29,103 @@ TEST_CASE("GraphNode", "[group7][graphnode]")
         CHECK(node.GetInput(1)->GetOutput() == 4);
         CHECK(node.GetOutput() == 0.0);
     }
+    SECTION("GraphNode function pointers")
+    {
+        GraphNode node;
+        // Function to sum the first two inputs
+        node.SetFunctionPointer([](const std::vector<double> &inputs) { return inputs.at(0) + inputs.at(1); });
+
+        // Not enough inputs
+        CHECK(node.GetOutput() == 0);
+
+        auto node1 = std::make_shared<GraphNode>(3);
+        node.AddInput(node1);
+        // Still not enough inputs
+        CHECK(node.GetOutput() == 0);
+
+        auto node2 = std::make_shared<GraphNode>(4);
+        node.AddInput(node2);
+        // Now we have enough inputs
+        CHECK(node.GetOutput() == 7);
+    }
+}
+
+TEST_CASE("GraphNode function set", "[group7][functionset]")
+{
+    GraphNode node;
+
+    SECTION("Sum") {
+        node.SetFunctionPointer(Sum);
+        node.AddInput(std::make_shared<GraphNode>(3));
+        CHECK(node.GetOutput() == 3);
+        node.AddInput(std::make_shared<GraphNode>(4));
+        CHECK(node.GetOutput() == 7);
+        node.AddInput(std::make_shared<GraphNode>(5));
+        CHECK(node.GetOutput() == 12);
+    }
+    SECTION("AnyEq") {
+        node.SetFunctionPointer(AnyEq);
+        node.AddInput(std::make_shared<GraphNode>(3));  // The value to check for equality
+        CHECK(node.GetOutput() == 0);
+        node.AddInput(std::make_shared<GraphNode>(4));  // None equal
+        CHECK(node.GetOutput() == 0);
+        node.AddInput(std::make_shared<GraphNode>(5));  // None equal
+        CHECK(node.GetOutput() == 0);
+        node.AddInput(std::make_shared<GraphNode>(3));  // One equal
+        CHECK(node.GetOutput() == 1);
+        node.AddInput(std::make_shared<GraphNode>(4));  // One equal
+        CHECK(node.GetOutput() == 1);
+    }
+    SECTION("And") {
+        node.SetFunctionPointer(And);
+        node.AddInput(std::make_shared<GraphNode>(1));  // True
+        CHECK(node.GetOutput() == 1);
+        node.AddInput(std::make_shared<GraphNode>(5));  // True
+        CHECK(node.GetOutput() == 1);
+        node.AddInput(std::make_shared<GraphNode>(-1));  // True
+        CHECK(node.GetOutput() == 1);
+        node.AddInput(std::make_shared<GraphNode>(0));  // False
+        CHECK(node.GetOutput() == 0);
+        node.AddInput(std::make_shared<GraphNode>(1));  // False
+        CHECK(node.GetOutput() == 0);
+    }
+    SECTION("Not") {
+        node.SetFunctionPointer(Not);
+        auto input = std::make_shared<GraphNode>();
+        node.AddInput(input);
+        CHECK(node.GetOutput() == 1);  // No inputs, but default to true (return 1)
+
+        input->SetOutput(1);  // True -> 0
+        CHECK(node.GetOutput() == 0);
+
+        input->SetOutput(0);  // False -> 1
+        CHECK(node.GetOutput() == 1);
+
+        input->SetOutput(10);  // True -> 0
+        CHECK(node.GetOutput() == 0);
+    }
+    SECTION("Gate") {
+        node.SetFunctionPointer(Gate);
+        auto input = std::make_shared<GraphNode>(5);
+        node.AddInput(input);
+        // One input
+        CHECK(node.GetOutput() == 0);
+
+        auto condition = std::make_shared<GraphNode>();
+        node.AddInput(condition);
+        // condition returns 0 => Condition is false
+        CHECK(node.GetOutput() == 0);
+
+        condition->SetOutput(1);
+        // condition returns 1 => Condition is true
+        CHECK(node.GetOutput() == 5);
+
+        input->SetOutput(10);
+        CHECK(node.GetOutput() == 10);
+
+        condition->SetOutput(0);
+        CHECK(node.GetOutput() == 0);
+    }
 }
 
 TEST_CASE("Graph", "[group7][graph]")
