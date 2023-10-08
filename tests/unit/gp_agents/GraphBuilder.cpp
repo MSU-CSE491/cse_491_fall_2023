56dab0e5613e6e3d884e3d9c09fbc0f731f72826
ssit <ssit5568@gmail.com>
2023-10-08 13:01:09 -0400

    Remove action map from GraphBuilder tests

--- tests/unit/gp_agents/GraphBuilder.cpp
+++ tests/unit/gp_agents/GraphBuilder.cpp
@@ -14,14 +14,14 @@ using namespace cowboys;
 TEST_CASE("Cartesian Graph", "[group7][graph][cartesian]") {
     SECTION("Cartesian Graph construction") {
         constexpr size_t INPUT_SIZE = 10000;
+        constexpr size_t NUM_OUTPUTS = 4;
         constexpr size_t NUM_LAYERS = 10;
         constexpr size_t NUM_NODES_PER_LAYER = 10;
         constexpr size_t NUM_NODE_INPUTS = 10;
         constexpr size_t LAYERS_BACK = 2;
-        auto action_map = std::unordered_map<std::string, size_t>{{"up", 0}, {"down", 1}, {"left", 2}, {"right", 3}};
         GraphBuilder builder;
 
-        auto graph = builder.CartesianGraph(INPUT_SIZE, action_map.size(), NUM_LAYERS, NUM_NODES_PER_LAYER);
+        auto graph = builder.CartesianGraph(INPUT_SIZE, NUM_OUTPUTS, NUM_LAYERS, NUM_NODES_PER_LAYER);
 
         // Input layer + middle layers + output layer
         size_t expected_layer_count = NUM_LAYERS + 2;
@@ -29,7 +29,7 @@ TEST_CASE("Cartesian Graph", "[group7][graph][cartesian]") {
 
         size_t num_input_nodes = INPUT_SIZE;
         size_t num_middle_nodes = NUM_LAYERS * NUM_NODES_PER_LAYER;
-        size_t num_output_nodes = action_map.size();
+        size_t num_output_nodes = NUM_OUTPUTS;
         size_t expected_node_count = num_input_nodes + num_middle_nodes + num_output_nodes;
         CHECK(graph->GetNodeCount() == expected_node_count);
     }
