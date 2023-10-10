3fc33dba9d726094286268504d3560e0fcfa8751
Richard Budny <budnyric@msu.edu>
2023-10-10 17:02:40 -0400

    More language work
    Tests + couple extra types

--- source/Worlds/Language.hpp
+++ source/Worlds/Language.hpp
@@ -23,5 +23,41 @@ namespace worldlang{
 	>
 	{};
 	
+	//[_a-zA-Z][_a-zA-Z0-9]
+	// (C-style identifier, case sensitive)
+	struct identifier : pegtl::identifier
+	{};
 	
+	// Match an operator
+	struct any_operator : pegtl::sor<
+		pegtl::one < '+', '-', '*', '/' >,
+//		pegtl::one < '&', '|', '^' >,
+		TAO_PEGTL_KEYWORD("&&"),
+		TAO_PEGTL_KEYWORD("||")
+	>
+	{};
+
+	// Must forward-declare for recursion
+	struct expression;
+
+	// Match single value	
+	struct element : pegtl::sor<
+		identifier,
+		number,
+		pegtl::seq<
+			pegtl::one< '(' >,
+			expression,
+			pegtl::one< ')' >
+		>
+	>
+	{};
+	
+	// Expressions:
+	// expr :=  e <op> e | e <op> expr
+	struct expression : pegtl::seq<
+		element,
+		any_operator,
+		element
+	>
+	{};
 } //worldlang
--- tests/unit/Worlds/Language.cpp
+++ tests/unit/Worlds/Language.cpp
@@ -10,6 +10,9 @@
 // Class project
 #include "Worlds/Language.hpp"
 
+// PEGTL grammar check
+#include "../third_party/PEGTL/include/tao/pegtl/contrib/analyze.hpp"
+
 // Macros to simplify case creation
 #define PEGTL_PARSE_CHECK(grammar, string, condition) {\
 	pegtl::memory_input in(string, "");\
@@ -18,6 +21,8 @@
 
 #define PARSE_TRUE(g, s) PEGTL_PARSE_CHECK(g, s, true)
 #define PARSE_FALSE(g, s) PEGTL_PARSE_CHECK(g, s, false)
+
+#define PEGTL_GRAMMAR_CHECK(grammar) CHECK(pegtl::analyze<grammar>(1) == 0);
 
 TEST_CASE("Basic compilation test", "[World][Language]"){
 	SECTION("Test compilation"){
@@ -28,11 +33,50 @@ TEST_CASE("Basic compilation test", "[World][Language]"){
 
 TEST_CASE("Langauge check", "[World][Langauge]"){
 	SECTION("Number rule"){
+		PEGTL_GRAMMAR_CHECK(worldlang::number);
+		
 		PARSE_TRUE(worldlang::number, "1234");
 		PARSE_TRUE(worldlang::number, "3.6");
 		PARSE_TRUE(worldlang::number, "746.876");
 		PARSE_TRUE(worldlang::number, "-0.4")
 		PARSE_FALSE(worldlang::number, "abcd");
 	}
+
+	SECTION("Identifier rule"){
+		PEGTL_GRAMMAR_CHECK(worldlang::identifier);
+		
+		PARSE_TRUE(worldlang::identifier, "abc");
+		PARSE_TRUE(worldlang::identifier, "abc123");
+		PARSE_TRUE(worldlang::identifier, "ABC");
+		PARSE_TRUE(worldlang::identifier, "_ident");
+		PARSE_TRUE(worldlang::identifier, "Z999");
+		PARSE_TRUE(worldlang::identifier, "name");
+
+		PARSE_FALSE(worldlang::identifier, "0abc");
+		PARSE_FALSE(worldlang::identifier, ".abc");
+		PARSE_FALSE(worldlang::identifier, "369");
+		PARSE_FALSE(worldlang::identifier, "!~!");
+	}
+
+	SECTION("Operator rules"){
+		PEGTL_GRAMMAR_CHECK(worldlang::any_operator);
+
+		PARSE_TRUE(worldlang::any_operator, "+");
+		PARSE_TRUE(worldlang::any_operator, "-");
+		PARSE_TRUE(worldlang::any_operator, "/");
+		PARSE_TRUE(worldlang::any_operator, "*");
+		PARSE_TRUE(worldlang::any_operator, "||");
+		PARSE_TRUE(worldlang::any_operator, "&&");
+
+		PARSE_FALSE(worldlang::any_operator, "a");
+		PARSE_FALSE(worldlang::any_operator, "0");
+		PARSE_FALSE(worldlang::any_operator, "`");
+	}
+
+	SECTION("Expression rules"){
+		PEGTL_GRAMMAR_CHECK(worldlang::expression);
+		
+		PARSE_TRUE(worldlang::expression, "3+5");
+	}	
 }
 
