0c132c8d66b4726ff1b1e8970d6ed7da485dec6e
YonesMussa <mussayon@msu.edu>
2023-10-11 00:00:00 -0400

    Adding tests for all the action and moving it around

--- source/DataCollection/DataReceiver.hpp
+++ source/DataCollection/DataReceiver.hpp
@@ -1,15 +1,9 @@
 #pragma once
 
 #include <vector>
-
 namespace DataCollection {
 
-    struct DataStorage
-    {
-        /* data */
-        std::vector<std::unordered_map<std::string, size_t>> actions;
 
-    };
     
     template <typename T>
     class DataReceiver {
@@ -17,14 +11,20 @@ namespace DataCollection {
     public:
     DataReceiver() = default;
     virtual ~DataReceiver() = default;
-    virtual void DebugPrint() = 0;
     inline void store_data(T obj) { storage.push_back(obj); }
-    inline void store_actions(std::unordered_map<std::string, size_t> actions){
-        DataStorage DataStorage;
-        DataStorage.actions.push_back(actions);
-        }
+    std::vector<T>& getStorage() {
+        return storage;
+    }
+    inline void store_actions(std::unordered_map<std::string, size_t> action){
+        actions.push_back(action);
+    }
+    auto &getActions(){
+        return actions;
+    }
+
     protected:
     std::vector<T> storage;
+    std::vector<std::unordered_map<std::string, size_t>> actions;
 
     };
 }
