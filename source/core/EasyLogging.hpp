de68c96ace04140c4cda24ad8004fafe7ff6e3bb
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2023-10-14 16:44:18 -0400

    updated with clang-format

--- source/core/EasyLogging.hpp
+++ source/core/EasyLogging.hpp
@@ -1,280 +1,262 @@
-#include <iostream>
-#include <sstream>
-#include <map>
 #include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
 
-namespace clogged{
+namespace clogged {
 
 /**
  * @brief Log levels for logging
- * 
+ *
  */
-    enum class LogLevel {
-        DEBUG,
-        INFO,
-        WARNING,
-        ERROR,
-        NA
-    };
+enum class LogLevel { DEBUG, INFO, WARNING, ERROR, NA };
 
 /**
  * @brief Teams Names for logging
- * 
+ *
  */
-    enum class Team {
-        TEAM_1,
-        TEAM_2,
-        TEAM_3,
-        TEAM_4,
-        TEAM_5,
-        TEAM_6,
-        TEAM_7,
-        TEAM_8,
-        TEAM_9,
-        GENERAL,
-        NA
-    };
+enum class Team {
+  TEAM_1,
+  TEAM_2,
+  TEAM_3,
+  TEAM_4,
+  TEAM_5,
+  TEAM_6,
+  TEAM_7,
+  TEAM_8,
+  TEAM_9,
+  GENERAL,
+  NA
+};
 
 /**
  * @brief Colors for logging
- * 
+ *
  */
-    enum class Color {
-        RESET = 0,
-        BLUE = 34,
-        GREEN = 32,
-        RED = 31
-    };
+enum class Color { RESET = 0, BLUE = 34, GREEN = 32, RED = 31 };
 
 /**
  * @brief Level of logging
  * @TODO: Change this to be a flag in the CMakeLists.txt
  */
-    const LogLevel LOGLEVEL = LogLevel::DEBUG;
-
+const LogLevel LOGLEVEL = LogLevel::DEBUG;
 
 /// @brief Ensure that we only log when NDEBUG flg is not set
 #ifndef NDEBUG
 
-#define LOGLINE \
-        "File: " << __FILE__ <<  "::->::Line(" << __LINE__ << ")"
+#define LOGLINE "File: " << __FILE__ << "::->::Line(" << __LINE__ << ")"
 
-#define RELATIVE_PATH(file) \
-    (std::string(file).find_last_of("/\\") != std::string::npos \
-        ? std::string(file).substr(std::string(file).find_last_of("/\\") + 1) \
-        : std::string(file))
+#define RELATIVE_PATH(file)                                                    \
+  (std::string(file).find_last_of("/\\") != std::string::npos                  \
+       ? std::string(file).substr(std::string(file).find_last_of("/\\") + 1)   \
+       : std::string(file))
 
-#define LOG_RELLINE \
-        "File: " << RELATIVE_PATH(__FILE__) << "::->::Line(" << __LINE__ << ")"
+#define LOG_RELLINE                                                            \
+  "File: " << RELATIVE_PATH(__FILE__) << "::->::Line(" << __LINE__ << ")"
 
-#define LOG_FNC \
-        "Function: " << __func__ << " "
+#define LOG_FNC "Function: " << __func__ << " "
 
 /**
  * @brief Logger class with colors and team names
  * @author @amantham20 @chatGPT
- * 
+ *
  * @details I was about to name this clogged.
- * @details This is a custom logger class that can be used to log messages to the console.
+ * @details This is a custom logger class that can be used to log messages to
+ * the console.
  */
-    class Logger {
-    public:
+class Logger {
+public:
+  /**
+   * @brief Sets the Team name for the current log
+   *
+   * @param team name of the team
+   * @return Logger&
+   */
+  Logger &operator<<(Team team) {
+    currentTeam = team;
+    metaPrinted = false;
+    //         std::cout << endl; //TODO: Might have to enable this so that we
+    //         can have same line logging when endl is not used
+    return *this;
+  }
 
-        /**
-         * @brief Sets the Team name for the current log
-         *
-         * @param team name of the team
-         * @return Logger&
-         */
-        Logger &operator<<(Team team) {
-            currentTeam = team;
-            metaPrinted = false;
-//         std::cout << endl; //TODO: Might have to enable this so that we can have same line logging when endl is not used
-            return *this;
-        }
+  /**
+   * @brief sets the log level for the current log
+   *
+   * @param logLevel Level/Type of the log
+   * @return Logger&
+   */
+  Logger &operator<<(LogLevel logLevel) {
+    currentLogLevel = logLevel;
+    metaPrinted = false;
+    return *this;
+  }
 
-        /**
-         * @brief sets the log level for the current log
-         *
-         * @param logLevel Level/Type of the log
-         * @return Logger&
-         */
-        Logger &operator<<(LogLevel logLevel) {
-            currentLogLevel = logLevel;
-            metaPrinted = false;
-            return *this;
-        }
+  /**
+   * @brief colors of the log
+   *
+   * @param color
+   * @return Logger&
+   */
+  Logger &operator<<(Color color) {
+    currentColor = color;
+    return *this;
+  }
 
+  /**
+   * @brief Manipulator for endl so that we can reset the values when a team is
+   * done logging
+   *
+   * @param manipulator
+   * @return Logger&
+   */
+  Logger &operator<<(std::ostream &(*manipulator)(std::ostream &)) {
 
-        /**
-         * @brief colors of the log
-         *
-         * @param color
-         * @return Logger&
-         */
-        Logger &operator<<(Color color) {
-            currentColor = color;
-            return *this;
-        }
+    typedef std::ostream &(*EndlManipulator)(std::ostream &);
 
-        /**
-         * @brief Manipulator for endl so that we can reset the values when a team is done logging
-         *
-         * @param manipulator
-         * @return Logger&
-         */
-        Logger &operator<<(std::ostream &(*manipulator)(std::ostream &)) {
+    // Compare the function pointers
+    if (manipulator == static_cast<EndlManipulator>(std::endl)) {
+      // Handle std::endl here
+      currentTeam = Team::NA;
+      currentLogLevel = LogLevel::DEBUG;
+      currentColor = Color::RESET;
+      std::cout << std::endl;
+    }
 
-            typedef std::ostream &(*EndlManipulator)(std::ostream &);
+    if (manipulator == endl) {
 
-            // Compare the function pointers
-            if (manipulator == static_cast<EndlManipulator>(std::endl)) {
-                // Handle std::endl here
-                currentTeam = Team::NA;
-                currentLogLevel = LogLevel::DEBUG;
-                currentColor = Color::RESET;
-                std::cout << std::endl;
-            }
+      currentTeam = Team::NA;
+      currentLogLevel = LogLevel::DEBUG;
+      currentColor = Color::RESET;
 
-            if (manipulator == endl) {
+      std::cout << std::endl; // TODO: Might have to enable this so that we can
+                              // have same line logging when endl is not used
+      metaPrinted = false;
+    }
+    return *this;
+  }
 
-                currentTeam = Team::NA;
-                currentLogLevel = LogLevel::DEBUG;
-                currentColor = Color::RESET;
+  /**
+   * @brief Aye I used a template.
+   * @brief Function to log the value
+   *
+   * @tparam T
+   * @param value
+   * @return Logger&
+   *
+   * @TODO: Might have to change this so that we only break a team log when a
+   * new team is set. aka ensure that  logger << Team::TEAM_1 << "Hello" <<
+   * "World" << endl; works in one line with one team print
+   */
+  template <typename T> Logger &operator<<(const T &value) {
 
-                std::cout
-                        << std::endl; //TODO: Might have to enable this so that we can have same line logging when endl is not used
-                metaPrinted = false;
-            }
-            return *this;
-        }
+    // TODO: Define when to log by loglevel comparison. Goal is to send it in as
+    // a flag in the CMakeLists.txt
+    if (currentLogLevel >= LOGLEVEL) {
 
-        /**
-         * @brief Aye I used a template.
-         * @brief Function to log the value
-         *
-         * @tparam T
-         * @param value
-         * @return Logger&
-         *
-         * @TODO: Might have to change this so that we only break a team log when a new team is set. aka ensure that  logger << Team::TEAM_1 << "Hello" << "World" << endl; works in one line with one team print
-         */
-        template<typename T>
-        Logger &operator<<(const T &value) {
-
-
-            //TODO: Define when to log by loglevel comparison. Goal is to send it in as a flag in the CMakeLists.txt
-            if (currentLogLevel >= LOGLEVEL) {
-
-                // added additional flag in case one wants to compile without colors (or) if the terminal does not support colors
+      // added additional flag in case one wants to compile without colors (or)
+      // if the terminal does not support colors
 #ifndef D_ANSI_COLOR_CODES
-                std::string colorStart = "\033[" + std::to_string(static_cast<int>(currentColor)) + "m";
-                std::string colorEnd = "\033[0m";
+      std::string colorStart =
+          "\033[" + std::to_string(static_cast<int>(currentColor)) + "m";
+      std::string colorEnd = "\033[0m";
 #else
-                std::string colorStart = "";
-                std::string colorEnd = "";
+      std::string colorStart = "";
+      std::string colorEnd = "";
 #endif
-                std::ostringstream logMessage;
-                logMessage << colorStart;
-                if (!metaPrinted) {
-                    logMessage << teamToString(currentTeam) << logToString(currentLogLevel);
-                    metaPrinted = true;
-                }
+      std::ostringstream logMessage;
+      logMessage << colorStart;
+      if (!metaPrinted) {
+        logMessage << teamToString(currentTeam) << logToString(currentLogLevel);
+        metaPrinted = true;
+      }
 
-                logMessage << value << colorEnd;
-                std::cout
-                        << logMessage.str(); // << std::endl;  //TODO: Might have to make enable this so that we can have same line logging when endl is not used
-            }
+      logMessage << value << colorEnd;
+      std::cout << logMessage.str(); // << std::endl;  //TODO: Might have to
+                                     // make enable this so that we can have
+                                     // same line logging when endl is not used
+    }
 
-            return *this;
-        }
+    return *this;
+  }
 
+  static Logger log; /// Global log instance //TODO: Check if poluting the
+                     /// global namespace is a good idea??
 
-        static Logger log; /// Global log instance //TODO: Check if poluting the global namespace is a good idea??
+  /**
+   * @brief Custom endl to reset the values
+   *
+   * @param os
+   * @return std::ostream&
+   */
+  static std::ostream &endl(std::ostream &os) {
 
-        /**
-         * @brief Custom endl to reset the values
-         *
-         * @param os
-         * @return std::ostream&
-         */
-        static std::ostream &endl(std::ostream &os) {
+    log << std::endl; // Call the custom Logger::endl to reset values
+    return os;
+  }
 
-            log << std::endl; // Call the custom Logger::endl to reset values
-            return os;
-        }
+private:
+  /// @brief Current team for that is going to log
+  Team currentTeam = Team::NA;
 
-    private:
-        /// @brief Current team for that is going to log
-        Team currentTeam = Team::NA;
+  /// @brief Current log level for the log
+  LogLevel currentLogLevel = LogLevel::DEBUG;
 
-        /// @brief Current log level for the log
-        LogLevel currentLogLevel = LogLevel::DEBUG;
+  /// @brief Current color for the log
+  Color currentColor = Color::RESET;
 
-        /// @brief Current color for the log
-        Color currentColor = Color::RESET;
+  bool metaPrinted = false;
 
-        bool metaPrinted = false;
+  /**
+   * @brief Map to convert Team enum to string
+   *
+   */
+  std::map<Team, std::string> teamToStringMap = {
+      {Team::TEAM_1, "Team 1"}, {Team::TEAM_2, "Team 2"},
+      {Team::TEAM_3, "Team 3"}, {Team::TEAM_4, "Team 4"},
+      {Team::TEAM_5, "Team 5"}, {Team::TEAM_6, "Team 6"},
+      {Team::TEAM_7, "Team 7"}, {Team::TEAM_8, "Team 8"},
+      {Team::TEAM_9, "Team 9"}, {Team::GENERAL, "General"}};
 
+  /**
+   * @brief Converts Team enum to string
+   *
+   * @param team
+   * @return std::string
+   */
+  std::string teamToString(Team team) {
 
-        /**
-         * @brief Map to convert Team enum to string
-         *
-         */
-        std::map<Team, std::string> teamToStringMap = {
-                {Team::TEAM_1,  "Team 1"},
-                {Team::TEAM_2,  "Team 2"},
-                {Team::TEAM_3,  "Team 3"},
-                {Team::TEAM_4,  "Team 4"},
-                {Team::TEAM_5,  "Team 5"},
-                {Team::TEAM_6,  "Team 6"},
-                {Team::TEAM_7,  "Team 7"},
-                {Team::TEAM_8,  "Team 8"},
-                {Team::TEAM_9,  "Team 9"},
-                {Team::GENERAL, "General"}
-        };
+    auto it = teamToStringMap.find(team);
+    if (it != teamToStringMap.end()) {
+      return "[" + it->second + "]";
+    }
 
-        /**
-         * @brief Converts Team enum to string
-         *
-         * @param team
-         * @return std::string
-         */
-        std::string teamToString(Team team) {
+    return "";
+  }
 
-            auto it = teamToStringMap.find(team);
-            if (it != teamToStringMap.end()) {
-                return "[" + it->second + "]";
-            }
-
-            return "";
-
-        }
-
-        /**
-         * @brief Converts LogLevel enum to string
-         *
-         * @param logLevel
-         * @return std::string
-         */
-        std::string logToString(LogLevel logLevel) {
-            if (logLevel == LogLevel::DEBUG) {
-                return "(DEBUG) ";
-            } else if (logLevel == LogLevel::INFO) {
-                return "(INFO) ";
-            } else if (logLevel == LogLevel::WARNING) {
-                return "(WARNING) ";
-            } else if (logLevel == LogLevel::ERROR) {
-                return "(ERROR) ";
-            } else {
-                return "";
-            }
-        }
-    };
-
+  /**
+   * @brief Converts LogLevel enum to string
+   *
+   * @param logLevel
+   * @return std::string
+   */
+  std::string logToString(LogLevel logLevel) {
+    if (logLevel == LogLevel::DEBUG) {
+      return "(DEBUG) ";
+    } else if (logLevel == LogLevel::INFO) {
+      return "(INFO) ";
+    } else if (logLevel == LogLevel::WARNING) {
+      return "(WARNING) ";
+    } else if (logLevel == LogLevel::ERROR) {
+      return "(ERROR) ";
+    } else {
+      return "";
+    }
+  }
+};
 
 /// @brief Global log instance
-    Logger Logger::log;
+Logger Logger::log;
 
 #else
 
@@ -282,26 +264,20 @@ namespace clogged{
 #define LOG_RELLINE ""
 #define LOG_FNC ""
 
+class Logger {
+public:
+  template <typename T> Logger &operator<<(const T &value) { return *this; }
 
-    class Logger {
-    public:
-        template <typename T>
-        Logger& operator<<(const T& value) {
-            return *this;
-        }
+  Logger &operator<<(std::ostream &(*manipulator)(std::ostream &)) {
+    return *this;
+  }
 
-        Logger &operator<<(std::ostream &(*manipulator)(std::ostream &)) {
-            return *this;
-        }
+  static std::ostream &endl(std::ostream &os) { return os; }
 
-        static std::ostream &endl(std::ostream &os) {
-            return os;
-        }
+  static Logger log;
+};
 
-        static Logger log;
-    };
-
-    Logger Logger::log;
+Logger Logger::log;
 #endif
 
-}  // namespace
+} // namespace clogged
