eb726963a7e69a1570dd1fd18b0f9d0d50cd114a
Ethan Rylko <ethan.rylko@gmail.com>
2023-10-17 00:01:17 -0400

    Don't do this

--- source/Interfaces/NetWorth/server/servermain.cpp
+++ source/Interfaces/NetWorth/server/servermain.cpp
@@ -14,6 +14,7 @@
 #include "../../../Worlds/MazeWorld.hpp"
 #include "../NetworkInterface.hpp"
 #include "networkingworld.hpp"
+#include "ServerPlayerInterface.hpp"
 
 Packet GridToPacket(const cse491::WorldGrid & grid, const cse491::type_options_t & type_options,
                   const cse491::item_set_t & item_set, const cse491::agent_set_t & agent_set)
@@ -70,10 +71,18 @@ int main()
     //Create the world on the server side upon initialization
     //Add the pacing agents to just simply walk around
     //Once everything is added we simply wait for a response from a client that is connecting.
+<<<<<<< Updated upstream
     cse491::netWorth::NetworkMazeWorld world;
     world.AddAgent<cse491::PacingAgent>("Pacer 1").SetPosition(3,1);
     world.AddAgent<cse491::PacingAgent>("Pacer 2").SetPosition(6,1);
     world.AddAgent<cse491::TrashInterface>("Interface").SetProperty("char", '@');
+=======
+    std::shared_ptr<netWorth::NetworkMazeWorld> world = std::make_shared<netWorth::NetworkMazeWorld>();
+    world->AddAgent<cse491::PacingAgent>("Pacer 1").SetPosition(3,1);
+    world->AddAgent<cse491::PacingAgent>("Pacer 2").SetPosition(6,1);
+    world->AddAgent<NetWorth::ServerPlayerInterface>("Interface").SetProperty("symbol", '@');
+    world->SetPlayerAgent();
+>>>>>>> Stashed changes
 
     UdpSocket serverSocket;
     serverSocket.bind(55002);
@@ -120,6 +129,21 @@ int main()
 
         }
 
+<<<<<<< Updated upstream
+=======
+        if (serverSocket->receive(recv_pkt, sender, port) != sf::Socket::Status::Done) {
+            std::cout << "Failure to receive" << std::endl;
+            return 1;
+        }
+
+        recv_pkt >> input;
+        std::cout << input << std::endl;
+
+        if (input == "quit") break;
+
+        world->SetPlayerAction(input);
+        world->RunAgents();
+>>>>>>> Stashed changes
     }
     
 }
