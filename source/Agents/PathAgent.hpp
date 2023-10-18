fa4fe2906b8a2622ee5e3490f8018bbb6a365954
davidrackerby <davidrackerby@gmail.com>
2023-10-17 20:55:59 -0400

    Slightly modify Path Agent interface to take no supplied path but require one later

--- source/Agents/PathAgent.hpp
+++ source/Agents/PathAgent.hpp
@@ -22,20 +22,19 @@ namespace walle {
  * Passed a sequence of to be sequentially applied as the agent is updated
  */
 class PathAgent : public cse491::AgentBase {
-protected:
+ protected:
   /// Collection of ways to offset the Agent's position
-  /// @attention This is a *not* a sequence of direct coordinates on the
-  /// WorldGrid, but a series of offsets
+  /// @attention This is a *not* a sequence of direct coordinates on the WorldGrid, but a series of offsets
   std::vector<cse491::GridPosition> offsets_;
 
   /// Current index into offsets_
   int index_ = 0;
 
-public:
+ public:
   PathAgent() = delete;
-  PathAgent(size_t id, std::string const &name,
-            std::vector<cse491::GridPosition> &&offsets);
-  PathAgent(size_t id, std::string const &name, std::string_view commands);
+  PathAgent(size_t id, std::string const& name);
+  PathAgent(size_t id, std::string const& name, std::vector<cse491::GridPosition> && offsets);
+  PathAgent(size_t id, std::string const& name, std::string_view commands);
   ~PathAgent() override = default;
 
   bool Initialize() override;
@@ -44,22 +43,20 @@ public:
   void DecrementIndex();
   [[nodiscard]] cse491::GridPosition GetNextPos() const;
 
-  virtual cse491::GridPosition UpdateAndGetNextPos(bool increment = true);
+  cse491::GridPosition UpdateAndGetNextPos(bool increment = true);
 
-  size_t SelectAction(cse491::WorldGrid const &, cse491::type_options_t const &,
-                      cse491::item_set_t const &,
-                      cse491::agent_set_t const &) override;
+  size_t SelectAction(cse491::WorldGrid const&, cse491::type_options_t const&, cse491::item_set_t const&, cse491::agent_set_t const&) override;
 
-  PathAgent &SetPath(std::vector<cse491::GridPosition> &&offsets,
-                     size_t start_index = 0);
-  PathAgent &SetPath(std::string_view commands, size_t start_index = 0);
+  PathAgent& SetPath(std::vector<cse491::GridPosition> && offsets, size_t start_index = 0);
+  PathAgent& SetPath(std::string_view commands, size_t start_index = 0);
 
   [[nodiscard]] int GetIndex() const;
-  [[nodiscard]] std::vector<cse491::GridPosition> const &GetPath() const;
+  [[nodiscard]] std::vector<cse491::GridPosition> const& GetPath() const;
+
 };
 
 std::vector<cse491::GridPosition> StrToOffsets(std::string_view commands);
 
 } // namespace walle
 
-#endif // GROUP_1_PROJECT_SOURCE_AGENTS_PATHAGENT_H_
+#endif //GROUP_1_PROJECT_SOURCE_AGENTS_PATHAGENT_H_
