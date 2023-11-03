39a3a59cfa0cbc0a465a63bf76ce0f3b27def494
ssit <ssit5568@gmail.com>
2023-11-03 16:28:15 -0400

    Update tests

--- tests/unit/gp_agents/Graph.cpp
+++ tests/unit/gp_agents/Graph.cpp
@@ -7,7 +7,9 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch_all.hpp>
 
+#include "Group7_GP_Agent/CGPGenotype.hpp"
 #include "Group7_GP_Agent/Graph.hpp"
+#include "Group7_GP_Agent/GraphBuilder.hpp"
 
 using namespace cowboys;
 
@@ -41,5 +43,22 @@ TEST_CASE("Graph", "[group7][graph]") {
     graph.AddLayer(layer2);
     CHECK(graph.GetLayerCount() == 2);
     CHECK(graph.GetNodeCount() == 4);
+  }
+
+  SECTION("Mutated Graph") {
+    CGPGenotype genotype({8, 4, 2, 10, 2});
+    GraphBuilder builder;
+    auto graph = builder.CartesianGraph(genotype, FUNCTION_SET);
+    auto action = graph->MakeDecision({1, 2, 3, 4, 5, 6, 7, 8}, {1, 2, 3, 4});
+    std::cout << action << std::endl;
+
+    bool all_same = true;
+    for (size_t i = 0; i < 10; ++i) {
+      genotype.MutateDefault(1);
+      graph = builder.CartesianGraph(genotype, FUNCTION_SET);
+      auto new_action = graph->MakeDecision({1, 2, 3, 4, 5, 6, 7, 8}, {1, 2, 3, 4});
+      all_same = all_same && (action == new_action);
+    }
+    CHECK(!all_same);
   }
 }
