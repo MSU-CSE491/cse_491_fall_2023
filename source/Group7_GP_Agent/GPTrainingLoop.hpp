04ca5fd833d50e01b71771e633ca0cd2d727a60b
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2023-11-03 12:15:09 -0400

    added a simple fitness function

--- source/Group7_GP_Agent/GPTrainingLoop.hpp
+++ source/Group7_GP_Agent/GPTrainingLoop.hpp
@@ -23,6 +23,10 @@ private:
     std::vector<cse491::WorldBase *> environments;
     std::vector<std::vector<cse491::AgentBase *>> agents;
 
+
+    std::vector<std::vector<cse491::GridPosition>> TEMPinitialAgentPositions ;
+    std::vector<std::vector<double>> TEMPAgentFitness ;
+
 public:
 
     GPTrainingLoop() {}
@@ -39,16 +43,33 @@ public:
             environments.emplace_back(new EnvironmentType());
 
             agents.emplace_back(std::vector<cse491::AgentBase *>());
+            TEMPinitialAgentPositions.emplace_back(std::vector<cse491::GridPosition>());
             for(size_t j = 0; j < NumAgentsForArena; ++j)
             {
 
                 cse491::AgentBase
                     &addedAgent = environments[i]->template AddAgent<AgentType>("Agent " + std::to_string(j));
+                cse491::GridPosition position = addedAgent.GetPosition();
+
+                TEMPinitialAgentPositions[i].emplace_back(position);
+
                 agents[i].emplace_back(&addedAgent);
             }
 
         }
 
+    }
+
+
+    double simpleFitnessFunction(cse491::AgentBase &agent, cse491::GridPosition startPosition)
+    {
+
+      cse491::GridPosition currentPosition = agent.GetPosition();
+
+      // Eucledian distance
+      double distance = std::sqrt(std::pow(currentPosition.GetX() - startPosition.GetX(), 2) + std::pow(currentPosition.GetY() - startPosition.GetY(), 2));
+
+      return distance;
     }
 
     void run(size_t numGenerations,
@@ -60,22 +81,6 @@ public:
 
         for(size_t generation = 0; generation < numGenerations; ++generation)
         {
-//            for(size_t arena = 0; arena < environments.size(); ++arena)
-//            {
-//                for(size_t turn = 0; turn < numberOfTurns; turn++)
-//                {
-//                    environments[arena]->RunAgents();
-//                    environments[arena]->UpdateWorld();
-//                }
-//            }
-
-//            std::vector<std::thread> threads;
-//
-//            for (size_t arena = 0; arena < environments.size(); ++arena)
-//            {
-//                threads.emplace_back(&GPTrainingLoop::runArena, this, arena, numberOfTurns);
-//            }
-
 
             std::vector<std::thread> threads;
 
@@ -100,7 +105,19 @@ public:
                 thread.join();
             }
 
-//            std::cout << "Generation " << generation << " complete" << std::endl;
+
+            // calculate fitness
+            for(size_t arena = 0; arena < environments.size(); ++arena)
+            {
+              TEMPAgentFitness.emplace_back(std::vector<double>());
+              for(size_t a = 0; a < agents[arena].size(); ++a)
+                {
+                    double fitness = simpleFitnessFunction(*agents[arena][a], TEMPinitialAgentPositions[arena][a]);
+                    TEMPAgentFitness[a].emplace_back(fitness);
+                }
+            }
+
+            std::cout << "Generation " << generation << " complete" << std::endl;
 
         }
     }
