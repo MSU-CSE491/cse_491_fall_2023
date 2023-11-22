eb0a4b188f68af6bc5c387dd4ff27223975d4a52
Yousif Murrani <murraniy@msu.edu>
2023-11-22 13:15:14 -0500

    Updated random agent and added some tests (incomplete for now)

--- tests/unit/Agents/RandomAgent.cpp
+++ tests/unit/Agents/RandomAgent.cpp
@@ -15,7 +15,7 @@ using namespace walle;
 
 TEST_CASE("Random Agent Initialization", "[Agents]") {
 
-    // create a star agent
+    // create random agent
     RandomAgent agent(1, "TestAgent");
 
     SECTION("Initialization Failure"){
@@ -35,3 +35,94 @@ TEST_CASE("Random Agent Initialization", "[Agents]") {
         REQUIRE(agent.Initialize() == true);
     }
 }
+
+TEST_CASE("Random Variable", "[Agents]"){
+
+    RandomAgent agent(1, "TestAgent");
+
+    agent.AddAction("up", 1);
+    agent.AddAction("down", 2);
+    agent.AddAction("left", 3);
+    agent.AddAction("right", 4);
+
+    // has actions so agent is initialized
+    REQUIRE(agent.Initialize() == true);
+
+    SECTION("Initial Random"){
+        // initial random variable
+        REQUIRE(agent.GetRandom() == 4.0);
+    }
+
+    SECTION("Calculate Random") {
+        // check random values multiple times
+        agent.CalculateRandom(4);
+        REQUIRE(agent.GetRandom() >= 0.0);
+        REQUIRE(agent.GetRandom() < 4.0);
+    }
+
+    SECTION("Set Random"){
+        // check setting random value
+        agent.SetDirection(1);
+        REQUIRE(agent.GetRandom() == 1.0);
+    }
+}
+
+TEST_CASE("Random Agent Moving", "[Agents]"){
+
+    RandomAgent agent(1, "TestAgent");
+
+    agent.AddAction("up", 1);
+    agent.AddAction("down", 2);
+    agent.AddAction("left", 3);
+    agent.AddAction("right", 4);
+
+    // has actions so agent is initialized
+    REQUIRE(agent.Initialize() == true);
+
+    SECTION("Initial Moving"){
+        REQUIRE(agent.GetMoving() ==true);
+    }
+
+    // set moving and check that it worked
+    agent.SetMoving(false);
+
+    SECTION("Set Moving"){
+        REQUIRE(agent.GetMoving() == false);
+    }
+}
+
+//TEST_CASE("Random Agent Select Actions", "[Agents]") {
+//
+//    RandomAgent agent(1, "TestAgent");
+//
+//    agent.AddAction("up", 1);
+//    agent.AddAction("down", 2);
+//    agent.AddAction("left", 3);
+//    agent.AddAction("right", 4);
+//
+//    // has actions so it is initialized
+//    REQUIRE(agent.Initialize() == true);
+//
+//    SECTION("Agent Moving") {
+//        // set moving true
+//        agent.SetMoving(true);
+//
+//        // select action many times to ensure it's within the valid range
+//        for (int i = 0; i < 100; ++i) {
+//            size_t selectedAction = agent.SelectAction({}, {}, {}, {});
+//            REQUIRE(selectedAction >= 1);
+//            REQUIRE(selectedAction <= 4);
+//        }
+//    }
+//
+//    SECTION("Agent Not Moving") {
+//        // set moving false
+//        agent.SetMoving(false);
+//
+//        // select action returns 0
+//        for (int i = 0; i < 100; ++i) {
+//            size_t selectedAction = agent.SelectAction({}, {}, {}, {});
+//            REQUIRE(selectedAction == 0);
+//        }
+//    }
+//}
