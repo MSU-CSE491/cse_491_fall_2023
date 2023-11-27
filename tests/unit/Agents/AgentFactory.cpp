76933aed8533c3e656ecd6b480a1fcc1c548cfea
Yousif Murrani <murraniy@msu.edu>
2023-11-27 13:26:10 -0500

    Base Agent Factory Test File, empty

---
+++ tests/unit/Agents/AgentFactory.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file AgentFactory.cpp
+ * @author Yousif Murrani
+ */
+
+// Catch2
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
+
+// class project
+// #include "Agents/AgentFactory.cpp"
+#include "Agents/RandomAgent.hpp"
+#include "Worlds/MazeWorld.hpp"
+
+using namespace walle;
