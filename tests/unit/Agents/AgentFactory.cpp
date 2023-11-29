836960bfe793c93d0a9ff86ba6a69187814b8353
Yousif Murrani <murraniy@msu.edu>
2023-11-29 12:59:29 -0500

    Agent Factory test file error

--- tests/unit/Agents/AgentFactory.cpp
+++ tests/unit/Agents/AgentFactory.cpp
@@ -8,8 +8,42 @@
 #include <catch2/catch_all.hpp>
 
 // class project
-// #include "Agents/AgentFactory.cpp"
-#include "Agents/RandomAgent.hpp"
+#include "Agents/AgentFactory.hpp"
 #include "Worlds/MazeWorld.hpp"
 
 using namespace walle;
+
+class MockWorld : public cse491::WorldBase {
+    int DoAction(cse491::AgentBase &agent, size_t action_id) override {}
+};
+
+TEST_CASE("Adding an AStarAgent"){
+
+    MockWorld world;
+    AgentFactory factory(world);
+
+    AStarAgentData agentData;
+    agentData.name = "AStarAgent1";
+    agentData.position = {0, 0};
+    agentData.symbol = '*';
+    agentData.target = {5, 5};
+    agentData.recalculate_after_x_turns = 3;
+
+    auto& agent = factory.AddAStarAgent(agentData);
+
+    SECTION("Properties") {
+        REQUIRE(agent.GetName() == agentData.name);
+        REQUIRE(agent.GetPosition() == agentData.position);
+        REQUIRE(agent.GetProperty<char>("symbol") == agentData.symbol);
+        REQUIRE(agent.GetGoalPosition() == agentData.target);
+        REQUIRE(agent.GetRecalculateValue() == agentData.recalculate_after_x_turns);
+    }
+
+    SECTION("Type") {
+        REQUIRE(dynamic_cast<walle::AStarAgent*>(&agent) != nullptr);
+    }
+}
+
+TEST_CASE("Adding an PacingAgent"){
+
+}
