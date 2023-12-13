71558b6ecffecaddbc4edb6eae5beb870a447628
Charles Ofria <charles.ofria@gmail.com>
2023-12-13 14:38:42 -0500

    Cleaned up DataCollection code.

--- source/DataCollection/DamageData.hpp
+++ source/DataCollection/DamageData.hpp
@@ -2,33 +2,37 @@
 
 #include <memory>
 #include <utility>
-#include "../core/Entity.hpp"
+
 #include "../core/AgentBase.hpp"
+#include "../core/Entity.hpp"
 
 namespace DataCollection {
 
-    /**
-     * @brief Represents damage related data between an agent and other entities (agents, items, grids, etc)
-     */
-    class DamageData {
-    private:
-        std::shared_ptr<cse491::AgentBase> agent;   /// The agent that took damage
-        std::shared_ptr<cse491::Entity> source;     /// The source entity that inflicted the damage
-        int amount;                                 /// The amount of damage taken from this source
+/**
+ * @brief Represents damage related data between an agent and other entities (agents, items, grids,
+ * etc)
+ */
+class DamageData {
+private:
+  std::shared_ptr<cse491::AgentBase> agent;  /// The agent that took damage
+  std::shared_ptr<cse491::Entity> source;    /// The source entity that inflicted the damage
+  int amount;                                /// The amount of damage taken from this source
 
-    public:
-        /**
-         * @brief Default constructor for a DamageData
-         * @param src Damage source entity
-         * @param amt Amount of damage taken
-         */
-        DamageData(std::shared_ptr<cse491::AgentBase> agnt,
-                   std::shared_ptr<cse491::Entity> src,
-                   int amt) : agent(std::move(agnt)), source(std::move(src)), amount(amt) {}
+public:
+  /**
+   * @brief Default constructor for a DamageData
+   * @param src Damage source entity
+   * @param amt Amount of damage taken
+   */
+  DamageData(std::shared_ptr<cse491::AgentBase> agnt, std::shared_ptr<cse491::Entity> src, int amt)
+      : agent(std::move(agnt)), source(std::move(src)), amount(amt)
+  {
+  }
 
-        /**
-        * @brief Destructor for DamageData class.
-        */
-        ~DamageData() = default;
-    };
-}
+  /**
+   * @brief Destructor for DamageData class.
+   */
+  ~DamageData() = default;
+};
+
+}  // namespace DataCollection
--- source/DataCollection/DamageReceiver.hpp
+++ source/DataCollection/DamageReceiver.hpp
@@ -1,17 +1,15 @@
 #pragma once
 
-#include "DataReceiver.hpp"
 #include "DamageData.hpp"
+#include "DataReceiver.hpp"
 
 namespace DataCollection {
 
-    /**
-     * @brief Data receiver class specialized for storing DamageData objects.
-     *
-     * This class extends DataReceiver class and provides specific functionality
-     * for storing DamageData objects along with damage sources and amounts.
-     */
-    class DamageReceiver : public DataReceiver<DamageData> {
-
-    };
-}
+/**
+ * @brief Data receiver class specialized for storing DamageData objects.
+ *
+ * This class extends DataReceiver class and provides specific functionality
+ * for storing DamageData objects along with damage sources and amounts.
+ */
+class DamageReceiver : public DataReceiver<DamageData> {};
+}  // namespace DataCollection
