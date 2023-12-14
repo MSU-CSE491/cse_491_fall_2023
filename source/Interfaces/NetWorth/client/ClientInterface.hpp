2a4dee71426695098649a58e4ee24fb35a905a58
Charles Ofria <charles.ofria@gmail.com>
2023-12-14 15:15:26 -0500

    Formated networking code.

--- source/Interfaces/NetWorth/client/ClientInterface.hpp
+++ source/Interfaces/NetWorth/client/ClientInterface.hpp
@@ -1,112 +1,111 @@
 /**
  * This file is part of the Fall 2023, CSE 491 course project.
  * @brief A networking interface that allows information to be sent across a  network
- * @note Status: PROTOTYPE
+ * @note Status: ALPHA
  **/
 
 #pragma once
 
+#include "ClientManager.hpp"
+#include "Interfaces/MainInterface.hpp"
 #include "Interfaces/NetWorth/NetworkInterface.hpp"
 #include "Interfaces/TrashInterface.hpp"
-#include "Interfaces/MainInterface.hpp"
-#include "ClientManager.hpp"
 
-namespace netWorth{
-    /**
-     * The interface of our client that will be interacting and connection with our server
-     */
-    using namespace sf;
+namespace netWorth {
+/**
+ * The interface of our client that will be interacting and connection with our server
+ */
+using namespace sf;
 
-    class ClientInterface : public NetworkingInterface, i_2D::MainInterface {
-        private:
-            netWorth::ClientManager *m_manager = nullptr;
+class ClientInterface : public NetworkingInterface, i_2D::MainInterface {
+private:
+  netWorth::ClientManager *m_manager = nullptr;
 
-        protected:
+public:
+  /**
+   * Default constructor (AgentBase)
+   * @param id agent ID
+   * @param name agent name
+   */
+  ClientInterface(size_t id, const std::string &name)
+      : cse491::InterfaceBase(id, name),
+        NetworkingInterface(id, name),
+        i_2D::MainInterface(id, name)
+  {
+  }
 
-        public:
-            /**
-             * Default constructor (AgentBase)
-             * @param id agent ID
-             * @param name agent name
-             */
-            ClientInterface(size_t id, const std::string & name) : cse491::InterfaceBase(id, name),
-                                                                   NetworkingInterface(id, name),
-                                                                   i_2D::MainInterface(id, name) {}
+  /**
+   * Establish connection with server, initializing interface
+   * @return True if successful, false if error
+   */
+  bool Initialize() override
+  {
+    // resolve port and IP from entity properties
+    m_ip = sf::IpAddress::resolve(NetworkingInterface::GetProperty<std::string>("server_ip"));
+    m_port = NetworkingInterface::GetProperty<unsigned short>("server_port");
+    m_manager = GetProperty<netWorth::ClientManager *>("manager");
+    m_manager->setupSocket(&m_socket, m_ip);
+    m_manager->setClientID(id);
 
-		/**
-		 * Establish connection with server, initializing interface
-		 * @return True if successful, false if error
-		 */
-            bool Initialize() override {
-                // resolve port and IP from entity properties
-                m_ip = sf::IpAddress::resolve(NetworkingInterface::GetProperty<std::string>("server_ip"));
-                m_port = NetworkingInterface::GetProperty<unsigned short>("server_port");
-                m_manager = GetProperty<netWorth::ClientManager *>("manager");
-				m_manager->setupSocket(&m_socket, m_ip);
-				m_manager->setClientID(id);
+    Packet sendPkt, recvPkt, twoPkt;
+    setMInputWaitTime(0.25f);
 
-                Packet sendPkt, recvPkt,twoPkt;
-                setMInputWaitTime(0.25f);
+    // send request message
+    sendPkt << "New client requesting connection.";
+    auto ip = m_ip.value();
+    if (!sendPacket(sendPkt, ip, m_port)) return false;
 
-                // send request message
-                sendPkt << "New client requesting connection.";
-                auto ip = m_ip.value();
-                if (!sendPacket(sendPkt, ip, m_port)) return false;
+    // receive from server
+    if (!receivePacket(recvPkt, m_ip, m_port)) return false;
+    // print received string (Connection established.)
+    std::string msg;
+    recvPkt >> msg;
+    std::cout << msg << std::endl;
 
-                // receive from server
-                if (!receivePacket(recvPkt, m_ip, m_port)) return false;
-                // print received string (Connection established.)
-                std::string msg;
-                recvPkt >> msg;
-                std::cout << msg << std::endl;
+    // request map to start send/receive loop
+    sendPkt.clear();
+    sendPkt << "Requesting start";
+    if (!sendPacket(sendPkt, m_ip.value(), m_port)) return false;
 
-                // request map to start send/receive loop
-                sendPkt.clear();
-                sendPkt << "Requesting start";
-                if (!sendPacket(sendPkt, m_ip.value(), m_port)) return false;
+    // receive action map from server for previous agents
+    receivePacket(recvPkt, m_ip, m_port);
+    m_manager->packetToActionMap(recvPkt);
 
+    return true;
+  }
 
-                // receive action map from server for previous agents
-				receivePacket(recvPkt, m_ip, m_port);
-				m_manager->packetToActionMap(recvPkt);
+  /**
+   * Choose action for player agent
+   * @param grid the client-side grid
+   * @param typeOptions different cell types of the world
+   * @param item_map the items that may be apart of the grid
+   * @param agent_map the agents that may be apart of the grid
+   * @return action ID of the interface
+   */
+  size_t SelectAction(const cse491::WorldGrid &grid, const cse491::type_options_t &typeOptions,
+                      const cse491::item_map_t &itemMap,
+                      const cse491::agent_map_t &agentMap) override
+  {
+    // Receive and draw map
+    sf::Packet sendPkt, recvPkt;
 
-                return true;
-            }
+    // grab action ID from MainInterface
+    size_t actionID = i_2D::MainInterface::SelectAction(grid, typeOptions, itemMap, agentMap);
 
-            /**
-             * Choose action for player agent
-             * @param grid the client-side grid
-             * @param typeOptions different cell types of the world
-             * @param item_map the items that may be apart of the grid
-             * @param agent_map the agents that may be apart of the grid
-             * @return action ID of the interface
-             */
-            size_t SelectAction(const cse491::WorldGrid & grid,
-                                const cse491::type_options_t & typeOptions,
-                                const cse491::item_map_t & itemMap,
-                                const cse491::agent_map_t & agentMap) override
-            {
-                // Receive and draw map
-                sf::Packet sendPkt, recvPkt;
+    // Send instruction to server
+    sendPkt << static_cast<uint64_t>(actionID);
+    sendPacket(sendPkt, m_ip.value(), m_port);
 
-                // grab action ID from MainInterface
-                size_t actionID = i_2D::MainInterface::SelectAction(grid, typeOptions,
-                            itemMap, agentMap);
+    m_manager->clearActionMap();
+    DrawGrid(grid, typeOptions, itemMap, agentMap);
 
-                // Send instruction to server
-                sendPkt << static_cast<uint64_t>(actionID);
-				sendPacket(sendPkt, m_ip.value(), m_port);
+    // await action map from server
+    receivePacket(recvPkt, m_ip, m_port);
+    m_manager->packetToActionMap(recvPkt);
 
-				m_manager->clearActionMap();
-                DrawGrid(grid, typeOptions, itemMap, agentMap);
+    // Do the action!
+    return actionID;
+  }
 
-                // await action map from server
-				receivePacket(recvPkt, m_ip, m_port);
-				m_manager->packetToActionMap(recvPkt);
-
-                // Do the action!
-                return actionID;
-            }
-
-    }; // End of ClientInterface
-} // End of namespace NetWorth
+};  // End of ClientInterface
+}  // namespace netWorth
--- source/Interfaces/NetWorth/server/ServerManager.hpp
+++ source/Interfaces/NetWorth/server/ServerManager.hpp
@@ -1,175 +1,183 @@
 /**
  * This file is part of the Fall 2023, CSE 491 course project.
  * @brief A networking interface that allows information to be sent across a  network
- * @note Status: PROTOTYPE
+ * @note Status: ALPHA
  **/
 
 #pragma once
-#include <thread>
 #include <map>
 #include <sstream>
+#include <thread>
 #include <utility>
 #include <vector>
+
 #include "Interfaces/NetWorth/NetworkInterface.hpp"
 
-namespace netWorth{
-    using namespace sf;
+namespace netWorth {
+using namespace sf;
 
-    /**
-     * The server that will be running and that allows clients to connect to
-     */
-    class ServerManager {
-    private:
+/**
+ * The server that will be running and that allows clients to connect to
+ */
+class ServerManager {
+private:
+  sf::UdpSocket m_manager_socket;
 
-		sf::UdpSocket m_manager_socket;
+  std::set<size_t> m_interface_set;  /// Set of interfaces on the server
 
-        std::set<size_t> m_interface_set;   /// Set of interfaces on the server
+  std::map<size_t, size_t> m_action_map;  /// Map of agent IDs to most recent action selected
 
-        std::map<size_t, size_t> m_action_map; ///Map of agent IDs to most recent action selected
+  std::string m_current_serialized_agents;  /// String with all current serialized agents
 
-        std::string m_current_serialized_agents; ///String with all current serialized agents
+  std::vector<std::pair<sf::IpAddress, unsigned short>>
+      m_update_vec;  /// IP Addresses and ports to send updates to
 
-		std::vector<std::pair<sf::IpAddress, unsigned short>> m_update_vec; ///IP Addresses and ports to send updates to
+  bool m_has_new_agent = false;  /// Boolean that states if this manager has gotten a new agent
 
-		bool m_has_new_agent = false; ///Boolean that states if this manager has gotten a new agent
+  bool m_interfaces_present =
+      false;  /// Boolean that states if there are interfaces present on the server
 
-		bool m_interfaces_present = false; ///Boolean that states if there are interfaces present on the server
+protected:
+public:
+  const static constexpr unsigned short m_init_connection_port =
+      55000;  /// Port for initial client connection
 
-    protected:
+  unsigned short m_max_client_port = 55000;  /// Port that is incremented for client thread handoff
 
-    public:
-        const static constexpr unsigned short m_init_connection_port = 55000; ///Port for initial client connection
+  /**
+   * Default constructor (AgentBase)
+   * @param id agent ID
+   * @param name agent name
+   */
+  ServerManager() = default;
 
-        unsigned short m_max_client_port = 55000; ///Port that is incremented for client thread handoff
+  /**
+   * Returns the current serialized agents
+   * @return a string of the current serialized agents
+   */
+  std::string getSerializedAgents() { return m_current_serialized_agents; }
 
-		/**
-		 * Default constructor (AgentBase)
-		 * @param id agent ID
-		 * @param name agent name
-		 */
-        ServerManager() = default;
+  /**
+   * Sets the current serialized agents of the server
+   * @param serializedAgents string reference representing the serialized agents
+   */
+  void setSerializedAgents(std::string& serializedAgents)
+  {
+    m_current_serialized_agents = serializedAgents;
+  }
 
-		/**
-		 * Returns the current serialized agents
-		 * @return a string of the current serialized agents
-		 */
-        std::string getSerializedAgents(){return m_current_serialized_agents;}
+  /**
+   * Returns if there are agents present on the server as a boolean
+   * @return boolean representing if there are agents present on the server
+   */
+  [[nodiscard]] bool hasAgentsPresent() const { return m_interfaces_present; }
 
-		/**
-		 * Sets the current serialized agents of the server
-		 * @param serializedAgents string reference representing the serialized agents
-		 */
-        void setSerializedAgents(std::string & serializedAgents) { m_current_serialized_agents = serializedAgents;}
+  /**
+   * Sets a boolean stating if the server has received a new agent
+   * @param hasNewAgent boolean stating that a new agent has joined
+   */
+  void setNewAgent(bool hasNewAgent) { m_has_new_agent = hasNewAgent; }
 
-		/**
-		 * Returns if there are agents present on the server as a boolean
-		 * @return boolean representing if there are agents present on the server
-		 */
-		[[nodiscard]] bool hasAgentsPresent() const {return m_interfaces_present;}
+  /**
+   * Convert action map to packet to send to client
+   * @return packet containing action map as series of integers
+   */
+  sf::Packet actionMapToPacket()
+  {
+    sf::Packet pkt;
 
-		/**
-		 * Sets a boolean stating if the server has received a new agent
-		 * @param hasNewAgent boolean stating that a new agent has joined
-		 */
-		void setNewAgent(bool hasNewAgent){ m_has_new_agent = hasNewAgent;}
+    // serialize action map
+    pkt << static_cast<uint64_t>(m_action_map.size());
+    for (auto [agent_id, action_id] : m_action_map) {
+      pkt << static_cast<uint64_t>(agent_id) << static_cast<uint64_t>(action_id);
+    }
 
-        /**
-         * Convert action map to packet to send to client
-         * @return packet containing action map as series of integers
-         */
-        sf::Packet actionMapToPacket()
-        {
-            sf::Packet pkt;
+    return pkt;
+  }
 
-            // serialize action map
-            pkt << static_cast<uint64_t>(m_action_map.size());
-            for (auto [agent_id, action_id]: m_action_map) {
-                pkt << static_cast<uint64_t>(agent_id) << static_cast<uint64_t>(action_id);
-            }
+  /**
+   * Increases the max client port
+   */
+  void increasePort() { ++m_max_client_port; }
 
-            return pkt;
+  /**
+   * Removes an interface by ID
+   * @param id
+   */
+  void removeInterface(size_t id)
+  {
+    m_interface_set.erase(id);
+    if (m_interface_set.empty()) m_interfaces_present = false;
+  }
+
+  /**
+   * Adds an IP and port to a vector for group update
+   * @param ip IP address of client receiving updates
+   * @param port port of client receiving updates
+   */
+  void addToUpdatePairs(sf::IpAddress ip, unsigned short port)
+  {
+    m_update_vec.emplace_back(ip, port);
+  }
+
+  /**
+   * Sends game updates to all clients when a new agent joins
+   */
+  void sendGameUpdates()
+  {
+    if (m_has_new_agent) {
+      sf::Packet serializedAgentPkt;
+      serializedAgentPkt << getSerializedAgents();
+      // Loops through all pairs of IP and port and sends serialized agents
+      for (auto client : m_update_vec) {
+        std::cout << "sending game updates to IP: " << client.first.toString() << " with port "
+                  << client.second << std::endl;
+        if (m_manager_socket.send(serializedAgentPkt, client.first, client.second) !=
+            sf::Socket::Status::Done) {
+          std::cerr << "Error sending updates to client at " << client.first.toString() << " port "
+                    << client.second << std::endl;
         }
+      }
+      m_has_new_agent = false;
+    }
+  }
 
-        /**
-         * Increases the max client port
-         */
-        void increasePort(){++m_max_client_port;}
+  /**
+   * Removes an interface from action map by key
+   * @param key
+   */
+  void removeFromActionMap(size_t key) { m_action_map.erase(key); }
 
-		/**
-		 * Removes an interface by ID
-		 * @param id
-		 */
-        void removeInterface(size_t id){
-            m_interface_set.erase(id);
-            if (m_interface_set.empty()) m_interfaces_present = false;
-        }
+  /**
+   * Removes ip and port from the update vector
+   * @param ip ip to remove
+   * @param port port to remove
+   */
+  void removeFromUpdatePairs(sf::IpAddress ip, unsigned short port)
+  {
+    m_update_vec.erase(std::remove_if(m_update_vec.begin(), m_update_vec.end(),
+                                      [ip, port](std::pair<sf::IpAddress, unsigned short> pair) {
+                                        return (pair.first == ip && pair.second == port);
+                                      }),
+                       m_update_vec.end());
+  }
 
-		/**
-		 * Adds an IP and port to a vector for group update
-		 * @param ip IP address of client receiving updates
-		 * @param port port of client receiving updates
-		 */
-		void addToUpdatePairs(sf::IpAddress ip, unsigned short port){
-			m_update_vec.emplace_back(ip, port);
-		}
+  /**
+   * Writes to the action map
+   * @param key to reference
+   * @param val to write
+   */
+  void writeToActionMap(size_t key, size_t val) { m_action_map.insert_or_assign(key, val); }
 
-		/**
-		 * Sends game updates to all clients when a new agent joins
-		 */
-		 void sendGameUpdates(){
-			if (m_has_new_agent)
-			{
-				sf::Packet serializedAgentPkt;
-				serializedAgentPkt << getSerializedAgents();
-				//Loops through all pairs of IP and port and sends serialized agents
-				for (auto client: m_update_vec){
-					std::cout << "sending game updates to IP: " << client.first.toString() << " with port " <<
-					client.second << std::endl;
-					if (m_manager_socket.send(serializedAgentPkt, client.first, client.second) != sf::Socket::Status::Done) {
-                        std::cerr << "Error sending updates to client at " << client.first.toString() << " port " << client.second << std::endl;
-                    }
-				}
-				m_has_new_agent = false;
-			}
-		}
+  /**
+   * Adds and interface to the interface set
+   * @param agent_id
+   */
+  void addToInterfaceSet(size_t agent_id)
+  {
+    m_interface_set.insert(agent_id);
+    m_interfaces_present = true;
+  }
 
-		/**
-		 * Removes an interface from action map by key
-		 * @param key
-		 */
-        void removeFromActionMap(size_t key){
-            m_action_map.erase(key);
-        }
-
-		/**
-		 * Removes ip and port from the update vector
-		 * @param ip ip to remove
-		 * @param port port to remove
-		 */
-		void removeFromUpdatePairs(sf::IpAddress ip, unsigned short port){
-			m_update_vec.erase(std::remove_if(m_update_vec.begin(), m_update_vec.end(),
-				[ip, port](std::pair<sf::IpAddress, unsigned short> pair){
-				return (pair.first == ip && pair.second == port);
-			}), m_update_vec.end());
-		}
-
-		/**
-		 * Writes to the action map
-		 * @param key to reference
-		 * @param val to write
-		 */
-        void writeToActionMap(size_t key, size_t val){
-            m_action_map.insert_or_assign(key, val);
-        }
-
-		/**
-		 * Adds and interface to the interface set
-		 * @param agent_id
-		 */
-        void addToInterfaceSet(size_t agent_id){
-            m_interface_set.insert(agent_id);
-			m_interfaces_present = true;
-        }
-
-    }; // End of class ServerManager
-} // End of namespace netWorth
+};  // End of class ServerManager
+}  // End of namespace netWorth
